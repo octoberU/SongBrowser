20dc8b5517d8f7728f171e615360ca2c6635297e
Meeps <cummies@meeps.me>
2020-10-13 01:55:06 -0700

    fixed Song Data Loader check
    CreateDisplayString() would throw an exception if Song Data Loader was not installed

--- AudicaMod/src/UI/AudicaScore/DifficultyDisplay.cs
+++ AudicaMod/src/UI/AudicaScore/DifficultyDisplay.cs
@@ -56,40 +56,82 @@ internal static class DifficultyDisplay
         difficultyText.text = CreateDisplayString(ScoreHistory.calculatedScores);
     }
 
+
+    //could be used for other info in the future
+    public static string CreateDisplayStringAdditions(SongDataLoader.SongData currentSong, string tagtofind, string textcolor)
+    {
+        if (currentSong.HasCustomData())
+        {
+            //if adding 360 tags
+            if (tagtofind == "easy360" || tagtofind == "advanced360" || tagtofind == "standard360" || tagtofind == "expert360")
+            {
+                if (currentSong.SongHasCustomDataKey(tagtofind))
+                {
+                    if(currentSong.GetCustomData<bool>(tagtofind))
+                    {
+                        return "<color=#" + textcolor + ">(360) </color>";
+                    }
+                }
+            }
+
+            //TODO: could do more tags here
+        }
+
+        return "";
+    }
+
+    private static void fillAdditions(string songID, string easyAdditions, string standardAdditions, string advancedAdditions, string expertAdditions)
+    {
+        SongDataLoader.SongData currentSong = SongDataLoader.AllSongData[songID];
+        easyAdditions = CreateDisplayStringAdditions(currentSong, "easy360", "b119f7");
+        advancedAdditions = CreateDisplayStringAdditions(currentSong, "advanced360", "f7a919");
+        standardAdditions = CreateDisplayStringAdditions(currentSong, "standard360", "19d2f7");
+        expertAdditions = CreateDisplayStringAdditions(currentSong, "expert360", "54f719");
+    }
+
     public static string CreateDisplayString(List<CalculatedScoreEntry> scores)
     {
         string output = "Difficulty Rating\n";
         var songData = SongDataHolder.I.songData;
         var calc = new DifficultyCalculator(songData);
 
-        bool hasCustomData = false;
-        SongDataLoader.SongData currentSong = SongDataLoader.AllSongData[songData.songID];
-        if (currentSong.HasCustomData())
-            hasCustomData = true;
+        string easyAdditions = "";
+        string standardAdditions = "";
+        string advancedAdditions = "";
+        string expertAdditions = "";
+
+        if (SongBrowser.songDataLoaderInstalled)
+        {
+            //fillAdditions() is separated into its own function to avoid errors if !songDataLoaderInstalled
+            /*Explanation:
+            When CreateDisplayString() is called the first time it loads in all the functions it needs.
+            If we include anything from the Song Data Loader utility in CreateDisplayString() it will try to
+            load from the dll. In the case they dont have the dll, it will throw an exception and the rest of
+            CreateDisplayString() won't run. Since all mentions of Son Data Loader are in fillAdditions(), which 
+            is behind a conditional we wont have that issue.
+            */
+            fillAdditions(songData.songID, easyAdditions, standardAdditions, advancedAdditions, expertAdditions);
+        }
 
         if (calc.expert != null)
         {
             output += $"<color=#b119f7>{calc.expert.difficultyRating.ToString("n2")}</color>  ";
-            if (hasCustomData && currentSong.SongHasCustomDataKey("easy360"))
-                output += " <color=#b119f7> (360) </color>";
+            output += expertAdditions;
         }
         if (calc.advanced != null)
         {
             output += $"<color=#f7a919>{calc.advanced.difficultyRating.ToString("n2")}</color>  ";
-            if (hasCustomData && currentSong.SongHasCustomDataKey("advanced360"))
-                output += " <color=#f7a919> (360) </color>";
+            output += advancedAdditions;
         }
         if (calc.standard != null)
         {
             output += $"<color=#19d2f7>{calc.standard.difficultyRating.ToString("n2")}</color>  ";
-            if (hasCustomData && currentSong.SongHasCustomDataKey("standard360"))
-                output += " <color=#19d2f7>(360) </color>";
+            output += standardAdditions;
         }
         if (calc.beginner != null)
         {
             output += $"<color=#54f719>{calc.beginner.difficultyRating.ToString("n2")}</color>  ";
-            if (hasCustomData && currentSong.SongHasCustomDataKey("expert360"))
-                output += " <color=#54f719> (360)</color>";
+            output += easyAdditions;
         }
         return output;
     }
