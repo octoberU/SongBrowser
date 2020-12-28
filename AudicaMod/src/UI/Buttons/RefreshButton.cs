3fa58be1d3bb29cfb97c61ef06c80a9fd4ccbfd5
Silzoid <degundemuradun@gmail.com>
2020-12-28 17:39:35 -0600

    Refresh button now moves user to main menu

--- AudicaMod/src/UI/Buttons/RefreshButton.cs
+++ AudicaMod/src/UI/Buttons/RefreshButton.cs
@@ -1,52 +1,32 @@
 ﻿using System;
-using Il2CppSystem.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 using Harmony;
-using MelonLoader;
-using System.Collections;
 
 namespace AudicaModding
 {
     internal static class RefreshButton
     {
-
         private static GameObject refreshButton;
-        public static bool exists => refreshButton != null;
 
-        private static Vector3 randomButtonPos = new Vector3(10.2f, -15.4f, 24.2f);
+        private static Vector3 randomButtonPos = new Vector3(10.4f, -1.5f, 0.0f);
         private static Vector3 randomButtonRot = new Vector3(0f, 0f, 0f);
-        private static SongSelect songSelect = null;
 
-
-        public static IEnumerator CreateRefreshButton()
+        public static void CreateRefreshButton()
         {
-            yield return new WaitForSeconds(.8f);
             if (refreshButton != null)
             {
                 refreshButton.SetActive(true);
-                yield break;
+                return;
             }
             var backButton = GameObject.Find("menu/ShellPage_Song/page/backParent/back");
-            refreshButton = GameObject.Instantiate(backButton, backButton.transform.parent.transform);
-            GameObject.Destroy(refreshButton.GetComponentInChildren<Localizer>());
-            TextMeshPro buttonText = refreshButton.GetComponentInChildren<TextMeshPro>();
-            buttonText.text = "Refresh Songs";
-            GunButton button = refreshButton.GetComponentInChildren<GunButton>();
-            button.destroyOnShot = true;
-            button.doMeshExplosion = true;
-            button.doParticles = true;
-            button.onHitEvent = new UnityEvent();
-            button.onHitEvent.AddListener(new Action(() => { OnRefreshButtonShot(); }));
-            refreshButton.transform.position = randomButtonPos;
-            refreshButton.transform.rotation = Quaternion.Euler(randomButtonRot);
+            refreshButton  = GameObject.Instantiate(backButton, backButton.transform.parent.transform);
+            ButtonUtils.InitButton(refreshButton, "Refresh Songs", new Action(() => { OnRefreshButtonShot(); }), 
+                                   randomButtonPos, randomButtonRot);
         }
         private static void OnRefreshButtonShot()
         {
+            GameObject.FindObjectOfType<MenuState>().GoToMainPage();
             SongBrowser.ReloadSongList();
-            refreshButton = null;
-            MelonCoroutines.Start(CreateRefreshButton());
         }
        
 
@@ -55,7 +35,7 @@ namespace AudicaModding
         {
             private static void Postfix(MenuState __instance, ref MenuState.State state)
             {               
-                if (state == MenuState.State.SongPage) MelonCoroutines.Start(CreateRefreshButton());
+                if (state == MenuState.State.SongPage) CreateRefreshButton();
             }
         }
 
